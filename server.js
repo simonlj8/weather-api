48bfc41401b8cea90b58a26f378ccd7755ed38e4
Simon Ljungström <simonlj8@gmail.com>
2024-11-25 20:29:34 +0100

    Changed API provider and add axios

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from "express"
 import 'dotenv/config';
+import axios from "axios";
 
 // require('dotenv').config();
 // const express = require('express');
 const app = express();
 
-const apiKey = process.env.API_KEY;
+const API_KEY = process.env.WEATHER_API_KEY;
+const BASE_URL = 'http://api.weatherapi.com/v1';
 const redisConnection = process.env.API_KEY;
 const PORT = process.env.PORT
 
@@ -13,10 +15,50 @@ app.get("/", (req, res) => {
     res.send("Nothing to see here");
 });
 
-app.get('/api', (req, res) => {
-    res.send("Nothing to see here");
+// app.get('/api', (req, res) => {
+//     res.send("Nothing to see here");
+// });
+
+async function getWeather(location) {
+    try {
+        const response = await axios.get(`${BASE_URL}/current.json`, {
+            params: {
+                key: API_KEY,
+                q: location, // Plats (t.ex. "Stockholm" eller "Umeå")
+                aqi: 'yes',   // Air Quality Index, sätt till "yes" om du vill ha AQI-data
+            },
+        });
+
+        const weatherData = response.data;
+        console.log(`Vädret i ${location}:`, weatherData);
+        return weatherData;
+    } catch (error) {
+        console.error('Kunde inte hämta väderdata:', error.message);
+    }
+}
+
+// Testa funktionen
+getWeather('UMEÅ');
+
+
+app.get('/api/weather/:location', async (req, res) => {
+    const location = req.params.location;
+    try {
+        const response = await axios.get(`${BASE_URL}/current.json`, {
+            params: {
+                key: API_KEY,
+                q: location,
+                aqi: 'yes',
+            },
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.log("Fel vid hämtning av väderdata:", error.message);
+        res.status(500).json({error: 'Kan inte hämrta väderdata'})
+    }
 });
 
+////// Hårdkodat för test
 app.get('/api/weather', (req, res) => {
     const weatherResponse = {
         location: "Umeå",
@@ -31,7 +73,7 @@ app.get('/api/weather', (req, res) => {
     res.json(weatherResponse);
 
 });
-
+///////
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
