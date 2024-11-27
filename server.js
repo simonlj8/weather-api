ddef6b65fda32ba20e32d042402476a9dcdde6a1
Simon Ljungström <simonlj8@gmail.com>
2024-11-27 20:30:15 +0100

    Delete som uncomment code

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express"
 import 'dotenv/config';
 import axios from "axios";
-import {rateLimit} from "express-rate-limit";
+import { rateLimit } from "express-rate-limit";
 import path from "path";
 import { fileURLToPath } from 'url';
 import cors from "cors";
@@ -18,7 +18,7 @@ app.set('trust proxy', 1);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-// Serve frontend build files
+
 app.use(express.static(path.join(__dirname, "build")));
 app.use(cors());
 
@@ -30,8 +30,8 @@ const limiter = rateLimit(
         standardHeaders: "draft-7",
         legacyHeaders: false,
         message: {
-        error: 'To many request, try again later.',
-    },
+            error: 'To many request, try again later.',
+        },
     }
 )
 
@@ -40,10 +40,6 @@ app.use('/api/weather', limiter)
 app.get("/", (req, res) => {
     res.send("Nothing to see here");
 });
-
-// app.get('/api', (req, res) => {
-//     res.send("Nothing to see here");
-// });
 
 async function getWeather(location) {
     try {
@@ -63,7 +59,6 @@ async function getWeather(location) {
     }
 }
 
-// Testa funktionen
 getWeather('UMEÅ');
 
 
@@ -80,36 +75,14 @@ app.get('/api/weather/:location', async (req, res) => {
         res.json(response.data);
     } catch (error) {
         console.log("Fel vid hämtning av väderdata:", error.message);
-        res.status(500).json({error: 'Kan inte hämrta väderdata'})
+        res.status(500).json({ error: 'Kan inte hämrta väderdata' })
     }
 });
 
 // Serve React frontend for all routes not starting with /api
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "build", "index.html"));
-  });
-
-////// Hardcoded for test
-// app.get('/api/weather', (req, res) => {
-//     const weatherResponse = {
-//         location: "Umeå",
-//         temperature: 2,
-//         condition: "Cloudy",
-//         windSpeed: 27,
-//         unit: "Celsius",
-//         timestamp: new Date().toISOString(),
-    
-//     };
-
-//     res.json(weatherResponse);
-
-// });
-///////
-
-
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "build", "index.html"))
-// })
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
